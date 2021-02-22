3f4bc8b0cf8ff0fb57271acc538ed3e904db1083
rikkyps <rikky.putra14@gmail.com>
2021-02-22 12:41:50 +0700

    revisi 3

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,24 @@
-$(function (){
-  $(document).scroll(function() {
-    var $nav = $('.navbar-fixed-top');
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-  })
-})
+
+
+$(document).ready(function () {
+
+  $('.counter').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()
+    }, {
+      duration: 2000,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now));
+      }
+    });
+  });
+
+});
+
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
