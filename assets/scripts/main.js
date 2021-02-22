e47d0889e6b1ab281eb84e0b9d7b69b22e510c25
rikkyps <rikky.putra14@gmail.com>
2021-02-23 03:03:16 +0700

    revisi 4 umkm profile is done

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,6 +14,27 @@ $(document).ready(function () {
     });
   });
 
+  $('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    responsiveClass: true,
+    
+    responsive: {
+      0: {
+        items: 2,
+        nav: false
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 5,
+        nav: false,
+      }
+    },
+  });
+
 });
 
 $(function () {
